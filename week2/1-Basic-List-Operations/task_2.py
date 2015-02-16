1f2829a03cc00221ca59db70f439a32635d51bd5
Kate <keiti93@users.noreply.github.com>
2015-02-16 13:20:13 +0200

    Create task_2.py

---
+++ week2/1-Basic-List-Operations/task_2.py
@@ -0,0 +1,15 @@
+languages = ["Python", "Ruby"]
+languages = languages + ["C++", "Java", "C#"]
+languages = ["Haskel"] + languages
+languages = languages + ["Go"]
+
+first = languages[0]
+second = languages[2]
+fifth = languages[4]
+languages[5]= "F#"
+last = languages[len(languages) - 1]
+
+"Haskel" in languages
+"Ruby" in languages
+"Go" in languages
+"Rust" in languages
