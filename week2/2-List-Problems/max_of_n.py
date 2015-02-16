d1024ee9406d0f7089d71ca88899ce0e736982d5
Kate <keiti93@users.noreply.github.com>
2015-02-16 13:30:11 +0200

    Create max_of_n.py

---
+++ week2/2-List-Problems/max_of_n.py
@@ -0,0 +1,19 @@
+n = input ("Enter n: ")
+n = int(n)
+
+numbers = []
+count = 1
+
+while count <= n:
+    number = input()
+    number = int(number)
+    count += 1
+    numbers = numbers + [number]
+    
+a = numbers[0]
+
+for number in numbers:
+    if number>a:
+        a = number
+
+print ("Max is:", a)
