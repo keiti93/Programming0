e091525568c6f17b979fc1aaeea9a27c1a1e3057
Kate <keiti93@users.noreply.github.com>
2015-02-16 14:29:26 +0200

    Create digits.py

---
+++ week2/4-Three-More-Problems/digits.py
@@ -0,0 +1,17 @@
+n = input ("Enter number: ")
+n = int(n)
+
+digits = []
+
+while n != 0:
+    digits = [n%10] + digits
+    n = n//10
+
+print (digits)
+
+number = 0
+
+for digit in digits:
+    number = number*10 + digit
+
+print (number)
