f0d7108e5a9273d9c16686407a621b109d1148a3
Kate <keiti93@users.noreply.github.com>
2015-02-16 12:34:37 +0200

    Create simple_answers.py

---
+++ week1/3-And-Or-Not-In-Problems/simple_answers.py
@@ -0,0 +1,11 @@
+a = input ("Enter text: ")
+
+if ("hello") in a or ("Hello")in a:
+    print ("Hello there, good stranger!")
+elif "how are you?" in a:
+    print ("I am fine, thanks. How are you?")
+elif "feelings" in a:
+    print ("I am a machine. I have no feelings.")
+elif "age" in a:
+    print ("I have no age. Only current timestamp.")
+    
