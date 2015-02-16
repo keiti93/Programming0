bea5eb2f7305a7425f21ca25a37096f38b2cfea2
Kate <keiti93@users.noreply.github.com>
2015-02-16 13:20:39 +0200

    Create task_1.py

---
+++ week2/1-Basic-List-Operations/task_1.py
@@ -0,0 +1,15 @@
+a = [1]
+
+b = [1, 2, 3]
+
+c = []
+
+d = [True, True, True] # :D
+
+e = ["Python", "Django"]
+
+f = ["Ekaterina", "Georgieva", "Stamatova"]
+
+g = ["Emo", "Desi", "Luben", "Stenli", "Magi"]
+
+h = ["C++", "C#", "HTML", "CSS", "Pascal", "Python"]
