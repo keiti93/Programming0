9788c900a0c68a9c797b2585cda69dbec3502121
Kate <keiti93@users.noreply.github.com>
2015-03-04 21:47:32 +0200

    Update magic.py

--- week5/4-Magic-Square/magic.py
+++ week5/4-Magic-Square/magic.py
@@ -1,6 +1,54 @@
+def sum_row(square):
+    general_sum = sum(square[0])
+    sum_r = 0
+            
+    for element in square:
+        if sum(element)!= general_sum:
+            return False
+    return True
+
+def sum_col(square):
+    general_sum = sum(square[0])
+    sum_column = 0
+            
+    for i in range (0, len(square)):
+        for j in range (0, len(square)):
+            sum_column += square[j][i]
+        if sum_column != general_sum:
+            return False
+        else:
+            sum_column = 0
+    return True
+
+def sum_d1(square):
+    general_sum = sum(square[0])
+    sum_d = 0
+    
+    for i in range (0, len(square)):
+        sum_d += square[i][i]
+    if sum_d != general_sum:
+        print(sum_d)
+        return False
+    return True
+
+def sum_d2(square):
+    general_sum = sum(square[0])
+    sum_d = 0
+    i = len(square) - 1
+    j = 0
+    
+    while i > -1:
+        sum_d += square[i][j]
+        i -= 1
+        j += 1
+
+    if sum_d != general_sum:
+        return False
+    return True
+
 def magic_square(square):
-    
-    if (sum(square[0]) == sum(square[1])) and (sum(square[1]) == sum(square[2])):
+    if sum_row(square) and sum_col(square) and sum_d1(square) and sum_d2(square):
         return True
-    
     return False
+    
+
