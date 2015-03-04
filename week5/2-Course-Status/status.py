a43c79273eeb2273b93fafab8c2d4b90da58a3d8
Kate <keiti93@users.noreply.github.com>
2015-03-04 20:06:28 +0200

    Update and rename week5 to week5/2-Course-Status/status.py

---
+++ week5/2-Course-Status/status.py
@@ -0,0 +1,14 @@
+def status_count(students):
+
+    result = {"finalized" : [],
+              "not_finalized" : []}
+              
+    for student in students:
+    
+        if student["status"] == "finalized":
+            result["finalized"] = result["finalized"] + [student["name"]]
+        else:
+        
+            result["not_finalized"] = result["not_finalized"] + [student["name"]]
+            
+    return result
